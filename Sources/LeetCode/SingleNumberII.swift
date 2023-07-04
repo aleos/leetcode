d8c26bedf1c82c54a6fe692f7c9d00eb1f57071d
Alexander Ostrovsky <thealeos@gmail.com>
2023-07-04 17:51:21 +0400

    Improve the solution for 137. Single Number II

--- Sources/LeetCode/SingleNumberII.swift
+++ Sources/LeetCode/SingleNumberII.swift
@@ -12,18 +12,14 @@
 /// - Complexity: You must implement a solution with a linear runtime complexity and use only constant extra space.
 class SingleNumberII {
     func singleNumber(_ nums: [Int]) -> Int {
-        let size = MemoryLayout<Int>.size * 8
-        var bitsCount = Array(repeating: 0, count: size)
+        var seenOnce = 0
+        var seenTwice = 0
         for num in nums {
-            for i in 0..<size {
-                bitsCount[i] += (num >> i) & 1
-            }
+            seenOnce = (seenOnce ^ num) & ~seenTwice
+            seenTwice = (seenTwice ^ num) & ~seenOnce
         }
-        bitsCount = bitsCount.map { $0 % 3 }
-        let result = (0..<size).reduce(into: 0) { partialResult, position in
-            partialResult |= bitsCount[position] << position
-        }
-        return result
+        
+        return seenOnce
     }
 }
 
