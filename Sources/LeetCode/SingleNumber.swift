52b7d1ab3764272a4f626ecfba108f6f44a4c692
Alexander Ostrovsky <thealeos@gmail.com>
2023-07-04 15:46:41 +0400

    Add a solution for 136. Single Number

---
+++ Sources/LeetCode/SingleNumber.swift
@@ -0,0 +1,21 @@
+//
+//  SingleNumber.swift
+//  
+//
+//  Created by Alexander Ostrovsky on 4/7/2023.
+//
+
+/// [136. Single Number](https://leetcode.com/problems/single-number/)
+///
+/// Given a non-empty array of integers `nums`, every element appears twice except for one. Find that single one.
+///
+/// - Complexity: You must implement a solution with a linear runtime complexity and use only constant extra space.
+class SingleNumber {
+    func singleNumber(_ nums: [Int]) -> Int {
+        var single = 0
+        for num in nums {
+            single ^= num
+        }
+        return single
+    }
+}
---
+++ Tests/LeetCodeTests/SingleNumberTests.swift
@@ -0,0 +1,49 @@
+//
+//  SingleNumberTests.swift
+//  
+//
+//  Created by Alexander Ostrovsky on 4/7/2023.
+//
+
+import XCTest
+@testable import LeetCode
+
+/// [136. Single Number](https://leetcode.com/problems/single-number/)
+///
+/// **Example 1**:
+///
+///     Input: nums = [2,2,1]
+///     Output: 1
+///
+/// **Example 2**:
+///
+///     Input: nums = [4,1,2,1,2]
+///     Output: 4
+///
+/// **Example 3**:
+///
+///     Input: nums = [1]
+///     Output: 1
+///
+/// **Constraints**:
+///
+/// - `1 <= nums.length <= 3 * 104`
+/// - `-3 * 104 <= nums[i] <= 3 * 104`
+/// - Each element in the array appears twice except for one element which appears only once.
+final class SingleNumberTests: XCTestCase {
+
+    func testExample1() throws {
+        let solution = SingleNumber()
+        XCTAssertEqual(solution.singleNumber([2,2,1]), 1)
+    }
+    
+    func testExample2() throws {
+        let solution = SingleNumber()
+        XCTAssertEqual(solution.singleNumber([4,1,2,1,2]), 4)
+    }
+
+    func testExample3() throws {
+        let solution = SingleNumber()
+        XCTAssertEqual(solution.singleNumber([1]), 1)
+    }
+}
