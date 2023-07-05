4812a89bbd3c67a08ebd9286f924e651a6e18d18
Alexander Ostrovsky <thealeos@gmail.com>
2023-07-05 23:32:55 +0400

    Add a solution for 1493. Longest Subarray of 1's After Deleting One Element

---
+++ Sources/LeetCode/1493.LongestSubarray.swift
@@ -0,0 +1,101 @@
+//
+//  1493.LongestSubarray.swift
+//  
+//
+//  Created by Alexander Ostrovsky on 5/7/2023.
+//
+
+/// [1493. Longest Subarray of 1's After Deleting One Element](https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/)
+///
+/// Given a binary array `nums`, you should delete one element from it.
+///
+/// Return the size of the longest non-empty subarray containing only `1`'s in the resulting array. Return `0` if there is no such subarray.
+class LongestSubarray {
+    func longestSubarray(_ nums: [Int]) -> Int {
+        var lastStart1Index: Int?
+        var last0Index: Int?
+        
+        var longest1sStartIndex: Int?
+        var longest1sEndIndex: Int?
+        var indexToDelete: Int?
+        
+        func len(start: Int, end: Int, zero: Int?) -> Int {
+            var len = end - start + 1
+            if let zero, zero > start && zero < end {
+                len -= 1
+            }
+            return len
+        }
+        
+        func updateLongest(start: Int, end: Int) {
+            if let oldLongest1sStartIndex = longest1sStartIndex, let oldLongest1sEndIndex = longest1sEndIndex {
+                let oldLen = len(start: oldLongest1sStartIndex, end: oldLongest1sEndIndex, zero: indexToDelete)
+                let newLen = len(start: start, end: end, zero: last0Index)
+                if newLen > oldLen {
+                    longest1sStartIndex = start
+                    longest1sEndIndex = end
+                    if let last0Index {
+                        indexToDelete = last0Index
+                    }
+                }
+            } else {
+                // The first found sequence of 1s
+                longest1sStartIndex = start
+                longest1sEndIndex = end
+                if let last0Index {
+                    indexToDelete = last0Index
+                }
+            }
+        }
+        
+        for (i, num) in nums.enumerated() {
+            if num == 0 {
+                if let last0Index {
+                    if let lastStart = lastStart1Index {
+                        if last0Index > lastStart {
+                            // The second 0. The current sequence is ended.
+                            if last0Index == i - 1 {
+                                // Previous num is also 0
+                                updateLongest(start: lastStart, end: last0Index - 1)
+                                lastStart1Index = nil
+                            } else {
+                                updateLongest(start: lastStart, end: i - 1)
+                                lastStart1Index = last0Index + 1
+                            }
+                        } else {
+                            // The first 0 in this 1s sequence. Continue
+                        }
+                    } else {
+                        // More than two 0s in a row
+                    }
+                } else {
+                    // The first 0 in this array
+                }
+                last0Index = i
+            } else {
+                if lastStart1Index == nil {
+                    lastStart1Index = i
+                }
+            }
+        }
+        
+        if let lastStart = lastStart1Index {
+            if let last0Index, last0Index == nums.count - 1 {
+                updateLongest(start: lastStart, end: last0Index - 1)
+            } else {
+                updateLongest(start: lastStart, end: nums.count - 1)
+            }
+        }
+
+        if let longest1sStartIndex, let longest1sEndIndex {
+            let len = len(start: longest1sStartIndex, end: longest1sEndIndex, zero: indexToDelete)
+            if indexToDelete != nil {
+                return len
+            } else {
+                return len - 1
+            }
+        } else {
+            return 0
+        }
+    }
+}
---
+++ Tests/LeetCodeTests/1493.LongestSubarrayTests.swift
@@ -0,0 +1,58 @@
+//
+//  1493.LongestSubarrayTests.swift
+//  
+//
+//  Created by Alexander Ostrovsky on 5/7/2023.
+//
+
+import XCTest
+@testable import LeetCode
+
+/// [1493. Longest Subarray of 1's After Deleting One Element](https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/)
+///
+/// **Example 1**:
+///
+///     Input: nums = [1,1,0,1]
+///     Output: 3
+///     Explanation: After deleting the number in position 2, [1,1,1] contains 3 numbers with value of 1's.
+///
+/// **Example 2**:
+///
+///     Input: nums = [0,1,1,1,0,1,1,0,1]
+///     Output: 5
+///     Explanation: After deleting the number in position 4, [0,1,1,1,1,1,0,1] longest subarray with value of 1's is [1,1,1,1,1].
+///
+/// **Example 3**:
+///
+///     Input: nums = [1,1,1]
+///     Output: 2
+///     Explanation: You must delete one element.
+///
+///
+/// **Constraints**:
+///
+/// - `1 <= nums.length <= 10^5`
+/// - `nums[i]` is either `0` or `1`.
+final class LongestSubarrayTests: XCTestCase {
+    
+    func testExample1() {
+        let solution = LongestSubarray()
+        XCTAssertEqual(solution.longestSubarray([1,1,0,1]), 3)
+    }
+    
+    func testExample2() {
+        let solution = LongestSubarray()
+        XCTAssertEqual(solution.longestSubarray([0,1,1,1,0,1,1,0,1]), 5)
+    }
+
+    func testExample3() {
+        let solution = LongestSubarray()
+        XCTAssertEqual(solution.longestSubarray([1,1,1]), 2)
+    }
+    
+    func testExample() {
+        let solution = LongestSubarray()
+        XCTAssertEqual(solution.longestSubarray([0,0,0,1,1,1,0,1,1,0,0,0,0,1,0,0,0,0,0,0]), 5)
+        XCTAssertEqual(solution.longestSubarray([1,1,0,0,1,1,1,0,1]), 4)
+    }
+}
