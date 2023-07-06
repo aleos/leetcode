ca527ec5c4875982e5f6161bb4388542a5546589
Alexander Ostrovsky <thealeos@gmail.com>
2023-07-06 16:56:15 +0400

    Add a solution to problem '209. Minimum Size Subarray Sum'

---
+++ Sources/LeetCode/209.MinSubArrayLen.swift
@@ -0,0 +1,36 @@
+//
+//  209.MinSubArrayLen.swift
+//  
+//
+//  Created by Alexander Ostrovsky on 6/7/2023.
+//
+
+/// [209. Minimum Size Subarray Sum](https://leetcode.com/problems/minimum-size-subarray-sum/)
+///
+/// Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray
+///  whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+class MinSubArrayLen {
+    
+    /// - Parameters:
+    ///   - target: A positive integer.
+    ///   - nums: An array of positive integers.
+    /// - Returns: The minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, returns 0 instead.
+    func minSubArrayLen(_ target: Int, _ nums: [Int]) -> Int {
+        var start = 0
+        var currentSum = 0
+        var minLength: Int = .max
+        
+        for (i, num) in nums.enumerated() {
+            currentSum += num
+            
+            while currentSum >= target {
+                // Found
+                minLength = min(minLength, i - start + 1)
+                currentSum -= nums[start]
+                start += 1
+            }
+        }
+        
+        return minLength == .max ? 0 : minLength
+    }
+}
---
+++ Tests/LeetCodeTests/209.MinSubArrayLenTests.swift
@@ -0,0 +1,49 @@
+//
+//  209.MinSubArrayLenTests.swift
+//  
+//
+//  Created by Alexander Ostrovsky on 6/7/2023.
+//
+
+import XCTest
+@testable import LeetCode
+
+/// [209. Minimum Size Subarray Sum](https://leetcode.com/problems/minimum-size-subarray-sum/)
+///
+/// **Example 1**:
+///
+///     Input: target = 7, nums = [2,3,1,2,4,3]
+///     Output: 2
+///     Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+///
+/// **Example 2**:
+///
+///     Input: target = 4, nums = [1,4,4]
+///     Output: 1
+///
+/// **Example 3**:
+///
+///     Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+///     Output: 0
+///
+/// **Constraints**:
+///
+/// - `1 <= target <= 109`
+/// - `1 <= nums.length <= 105`
+/// - `1 <= nums[i] <= 104`
+final class MinSubArrayLenTests: XCTestCase {
+    let solution = MinSubArrayLen()
+    
+    func testExample1() {
+        XCTAssertEqual(solution.minSubArrayLen(7, [2,3,1,2,4,3]), 2)
+    }
+    
+    func testExample2() {
+        XCTAssertEqual(solution.minSubArrayLen(4, [1,4,4]), 1)
+    }
+    
+    func testExample3() {
+        XCTAssertEqual(solution.minSubArrayLen(11, [1,1,1,1,1,1,1,1]), 0)
+    }
+    
+}
