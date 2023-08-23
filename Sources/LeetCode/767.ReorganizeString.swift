c86d77160e6f15ce755cb838199207c7d5ff8a50
Alexander Ostrovsky <thealeos@gmail.com>
2023-08-23 17:14:06 +0400

    Improve solution of '767. Reorganize String'
    https://leetcode.com/problems/reorganize-string/

--- Sources/LeetCode/767.ReorganizeString.swift
+++ Sources/LeetCode/767.ReorganizeString.swift
@@ -17,32 +17,27 @@ class ReorganizeString {
     /// - Parameter s: A string.
     /// - Returns: Any possible rearrangement of `s` or return `""` if not possible.
     func reorganizeString(_ s: String) -> String {
-        let frequences = s.reduce(into: [:]) { f, c in
-            f[c, default: 0] += 1
+        let length = s.count
+        let frequences = Array(s.reduce(into: [:]) { f, c in f[c, default: 0] += 1 })
+        let maxFreq = frequences.max(by: { $0.value < $1.value })!
+        guard maxFreq.value <= (length + 1) / 2 else { return "" }
+        
+        var result = Array(s)
+        var i = 0
+        for _ in 0..<maxFreq.value {
+            result[i] = maxFreq.key
+            i += 2
         }
-        var sortedFrequences = frequences.sorted { $0.value > $1.value }
-        
-        guard sortedFrequences.first!.value <= s.count + 1 else { return "" }
-        
-        var result = ""
-        while !sortedFrequences.isEmpty {
-            guard result.last != sortedFrequences[0].key else { return "" }
-            
-            result.append(sortedFrequences[0].key)
-            sortedFrequences[0].value -= 1
-            if sortedFrequences.count > 1 {
-                result.append(sortedFrequences[1].key)
-                sortedFrequences[1].value -= 1
-                if sortedFrequences[1].value == 0 {
-                    sortedFrequences.remove(at: 1)
+        for letter in frequences where letter.key != maxFreq.key {
+            for _ in 0..<letter.value {
+                if i >= length {
+                    i = 1
                 }
+                result[i] = letter.key
+                i += 2
             }
-            if sortedFrequences[0].value == 0 {
-                sortedFrequences.remove(at: 0)
-            }
-            sortedFrequences = sortedFrequences.sorted { $0.value > $1.value }
         }
         
-        return result
+        return String(result)
     }
 }
