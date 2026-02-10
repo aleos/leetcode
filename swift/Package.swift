203ffd310f8e07af5d61a699466918b9d8e9161c
Alexander Ostrovsky <alex@aleos.dev>
2026-02-10 23:12:15 +0530

    Add multilanguage support
    Move SPM project inside swift directory. If I add a new language, I create a new folder on the top level

--- swift/Package.swift
+++ swift/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "LeetCode",
+//    platforms: [.macOS(.v26), .iOS(.v26), .tvOS(.v26), .watchOS(.v26), .visionOS(.v26)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
