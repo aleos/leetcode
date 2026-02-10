d7233165b39e6f2f4943ce27f3050d23d877ccd0
Alexander Ostrovsky <alex@aleos.dev>
2026-02-11 01:14:39 +0530

    Add templates

---
+++ swift/_templates/PN.ProblemName.swift
@@ -0,0 +1,13 @@
+/// <#Problem Description#>
+///
+/// **Difficulty:** <#Easy/Medium/Hard#>
+///
+/// **Topics:** <#Topics#>
+///
+/// - SeeAlso: [<#PN#>. <#Problem Name#>](https://leetcode.com/problems/<#problem-slug#>/)
+public enum <#PNNNN#> {
+    /// - Complexity:
+    ///   **Time**: O(*<#...#>*), where *<#n#>* is <#...#> ,\
+    ///   **Space**: O(*<#...#>*), where *<#n#>* is <#...#>.
+
+}
---
+++ swift/_templates/PN.ProblemNameTests.swift
@@ -0,0 +1,31 @@
+import Testing
+
+@testable import LeetCode
+
+/// [<#PN#>. <#Problem Name#>](https://leetcode.com/problems/<#problem-slug#>/)
+///
+/// ## Constraints
+/// - <#Constraint 1#>
+/// - <#Constraint 2#>
+@Suite("<#NNNN#>. <#Problem Name#>")
+struct P<#NNNN#>Tests {
+
+    /// ## Example 1:
+    /// - Input: `<#Input#>`
+    /// - Output: `<#Output#>`
+    /// - Explanation: <#Explanation#>
+    @Test("Example 1: <#Scenario#>")
+    func example1() {
+        #expect(P<#NNNN#>.<#function#>(<#Input#>) == <#Output#>)
+    }
+
+    /// ## Example 2:
+    /// - Input: `<#Input#>`
+    /// - Output: `<#Output#>`
+    /// - Explanation: <#Explanation#>
+    @Test("Example 2: <#Scenario#>")
+    func example2() {
+        #expect(P<#NNNN#>.<#function#>(<#Input#>) == <#Output#>)
+    }
+
+}
