1f89935d19ac82a2ff0c09ef2ea5fca54f463e2f
Alexander Ostrovsky <alex@aleos.dev>
2026-02-10 16:47:05 +0530

    Bump up the swift version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "LeetCode",
-            targets: ["LeetCode"]),
+            targets: ["LeetCode"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
