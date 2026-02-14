cdd40e2599a0b04c3f148e2c3de5057b62b42d74
Alexander Ostrovsky <alex@aleos.dev>
2026-02-14 19:30:40 +0530

    Fix docstring in solution for problem 1929

--- swift/Sources/LeetCode/1929.ConcatenationOfArray.swift
+++ swift/Sources/LeetCode/1929.ConcatenationOfArray.swift
@@ -1,5 +1,8 @@
 /// Concatenation of Array
 ///
+/// Given an integer array `nums` of length `n`, you want to create an array `ans` of length `2n`
+/// where `ans[i] == nums[i]` and `ans[i + n] == nums[i]` for `0 <= i < n`.
+///
 /// **Difficulty:** Easy
 ///
 /// **Topics:** Mid Level, Array, Simulation, Weekly Contest 249
@@ -7,13 +10,13 @@
 /// - Precondition:
 ///   - `n == nums.length`
 ///   - `1 <= n <= 1000`
-///   - `1 <= nums[i] <= 1000
+///   - `1 <= nums[i] <= 1000`
 ///
-/// - SeeAlso: [Q1. Concatenation of Array](https://leetcode.com/problems/concatenation-of-array/)
+/// - SeeAlso: [1929. Concatenation of Array](https://leetcode.com/problems/concatenation-of-array/)
 public enum P1929 {
     /// - Complexity:
-    ///   **Time**: O(*n*), where *n* is nums ,\
-    ///   **Space**: O(*n*), where *n* is nums.
+    ///   **Time**: O(*n*), where *n* is the length of the array,\
+    ///   **Space**: O(*n*), where *n* is the length of the array.
     public static func getConcatenation(_ nums: [Int]) -> [Int] {
         nums + nums
     }
