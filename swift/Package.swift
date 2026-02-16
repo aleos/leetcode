8fd744a486d4ebb70099383f652abf71a0d31ab3
Alexander Ostrovsky <alex@aleos.dev>
2026-02-16 19:47:57 +0530

    Add swift-algorithms, swift-collections, and swift-numerics dependencies

--- swift/Package.swift
+++ swift/Package.swift
@@ -14,14 +14,21 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.5")
+        .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0"),
+        .package(url: "https://github.com/apple/swift-collections", from: "1.1.4"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.5"),
+        .package(url: "https://github.com/apple/swift-numerics", from: "1.0.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LeetCode",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Algorithms", package: "swift-algorithms"),
+                .product(name: "Collections", package: "swift-collections"),
+                .product(name: "Numerics", package: "swift-numerics"),
+            ]),
         .testTarget(
             name: "LeetCodeTests",
             dependencies: ["LeetCode"]),
